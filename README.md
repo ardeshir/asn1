687174fe157c593670dbace632402a4c3570dace
ardeshir sepahsalar <ardeshir.org@gmail.com>
2017-06-21 23:58:41 +0000

    added newstr

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ASN1 Serializer
+
+0> go run main.go                                                                                                                                              
+Before marshalling:  2017-06-21 23:58:09.292276525 +0000 UTC
+Marshalled time ok!
+After marshal/unmarshal time:  2017-06-21 23:58:09 +0000 UTC
+Before marshalling string:  hello ¼
+Marshalled string ok!
+After marshal/unmarshal new string:  hello ¼
--- main.go
+++ main.go
@@ -16,13 +16,25 @@ func main() {
 
 	mdata, err := asn1.Marshal(t)
         ckErr(err)
-        fmt.Println("Marshall ok!")
+        fmt.Println("Marshalled time ok!")
         
         var newtime = new(time.Time)
         _, err1 := asn1.Unmarshal(mdata, newtime)
         ckErr(err1)
-        fmt.Println("After marshal/unmarshal: ", newtime.String())
+        fmt.Println("After marshal/unmarshal time: ", newtime.String())
+       
+        s := "hello \u00bc"
+        fmt.Println("Before marshalling string: ", s)
         
+        mdata1, err := asn1.Marshal(s)
+        ckErr(err)
+        fmt.Println("Marshalled string ok!")
+       
+        var newstr string
+        _ , err2 := asn1.Unmarshal(mdata1, &newstr)
+        ckErr(err2)
+
+        fmt.Println("After marshal/unmarshal new string: ", newstr)        
 }
 
 func ckErr(err error) {
